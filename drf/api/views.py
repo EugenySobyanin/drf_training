e6053d7b50e52c66bc635a6b581fb6e2c66507b3
Евгений Собянин <ea.sobyanin2018@gmail.com>
2025-03-22 19:22:40 +0300

    ююю

--- drf/api/views.py
+++ drf/api/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import  get_object_or_404, render
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -23,11 +23,36 @@ class TagListCreateApiView(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
+# Получение одного тега, изменение, удаление
 class TagRetriveUpdateDeleteApiView(APIView):
+
+    def get_obj(self, pk):
+        return get_object_or_404(Tag, pk=pk)
+
     def get(self, request, pk):
-        tag = Tag.objects.get(pk=pk)
+        tag = self.get_obj(pk)
         serializer = TagSerializer(tag)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     def put(self, request, pk):
-        ...
+        tag = self.get_obj(pk)
+        serializer = TagSerializer(data=request.data, instance=tag)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def patch(self, request, pk):
+        tag = self.get_obj(pk)
+        serializer = TagSerializer(
+            data=request.data, instance=tag, partial=True
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def delete(self, request, pk):
+        tag = self.get_obj(pk)
+        tag.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
